b0ab3d696f519806bd5f1123f869c12ea7c8bcea
Guilherme <guilhermepm164@gmail.br>
2021-02-24 01:44:31 -0300

    request capturada pelo controler

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
 import "reflect-metadata"
-import express, { response } from 'express'
+import express from 'express'
 import "./database"
-let server = express()
+import { router } from "./routes"
+const server = express()
+server.use(express.json());
+server.use(router)
 
 
-server.get("/user", (require,response)=>{
-   return response.json(
-       {
-           name:"Guilherme",
-           email:"forestus7@gmail.com",
-           password: "******",
-           office:"Dev Junior Java"
-       }
-   ) 
-})
-
-server.post("/users/:id",(require,response)=>{
-    return
-})
-
-
-
-server.listen(3333,() =>console.log("server on!"))
+server.listen(3333, () => console.log("server on!"))
