250988373ea39464233421c38cc3490f407a8524
Guilherme <guilhermepm164@gmail.br>
2021-02-24 02:29:57 -0300

    entidade criada

---
+++ src/models/User.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+
+    @PrimaryColumn()
+    readonly id: string; // impede que o user tenha acesso a modificação desse valor
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @CreateDateColumn()
+    created_at: date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+export { User };
